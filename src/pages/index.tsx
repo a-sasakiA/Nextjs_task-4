fd30e7b63ba1fb973b473779429e22c43807b4a8
佐々木 杏 <a-sasaki@A-011.local>
2022-06-07 18:16:19 +0900

    初期セットアップ

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+import { Template } from "../components/templates";
+
+const Home: NextPage = () => (
+    <Template title="test">
+      <div>
+        <p>Test</p>
+      </div>
+    </Template>
+  );
+
+export default Home;
